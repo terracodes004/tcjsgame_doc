5b92b67da2120a00acb7acf165796b1f4f8956eb
Terra Codes <kehindeowolabi004@gmail.com>
2025-10-13 01:38:47 +0100

    Update speed.js

--- mat/speed.js
+++ mat/speed.js
@@ -1,11 +1,12 @@
-Display.prototype.interval = ani
+Display.prototype.interval = ani()
 
 function ani(){
     Mouse.x = mouse.x
         Mouse.y = mouse.y
-        this.frameNo += 1;
-        this.context.save();
-        this.context.translate(-this.camera.x, -this.camera.y);
+    display.stop()
+        display.frameNo += 1;
+        display.context.save();
+        display.context.translate(-this.camera.x, -this.camera.y);
         try {
             update();
         } catch (e) {
@@ -26,3 +27,4 @@ function ani(){
         return requestAnimationFrame(ani)
 
 }
+
